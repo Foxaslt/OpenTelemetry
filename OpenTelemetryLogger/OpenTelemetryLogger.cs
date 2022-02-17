1054baebb8c1c5136050cde77dcb3d6d1ca2e753
Romas Opulskis <opulskr@gmail.com>
2022-02-17 16:05:35 +0200

    Add project files.

---
+++ OpenTelemetryLogger/OpenTelemetryLogger.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Threading.Tasks;
+
+namespace OpenTelemetryLogger
+{
+    public class OpenTelemetryAttribute : ActionFilterAttribute
+    {
+        readonly Meter meter;
+        readonly Histogram<long> histogram;
+
+        public OpenTelemetryAttribute(string meterName, string gaugeName)
+        {
+            meter = new Meter(meterName);
+            histogram = meter.CreateHistogram<long>(gaugeName);
+        }
+
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var tag = new System.Collections.Generic.KeyValuePair<string, object>("context", context);
+
+            var stopWatch = Stopwatch.StartNew();
+            await next();
+
+            histogram.Record(stopWatch.ElapsedMilliseconds, tag);
+        }
+    }
+}
